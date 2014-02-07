ae76e89d94206e31536d49d2f1ad36213a6ecd3b
Koda <zaskoda@gmail.com>
2014-02-07 11:47:10 -0700

    initial LCARS style added

--- departments/views.py
+++ departments/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.template import RequestContext, loader
 # Create your views here.
+	
+@login_required
 def index(request):
-    return HttpResponse("Hello, world. You're at the shifts index.")
+    template = loader.get_template('departments/index.html')
+    context = RequestContext(request, {})
+    return HttpResponse(template.render(context))
 
 def detail(request, department_id):
     return HttpResponse("You're looking at department %s." % department_id)    
--- koda-notes.md
+++ koda-notes.md
@@ -1,4 +1,5 @@
  + How do static files need to be configured/served on launch?
  + Am I calling templates the best way i should?
  + Do I really need all of html5boilerplate?
- 
+ + The "Departments" model should populate a default option. Shifts should require a department.
+ + 
