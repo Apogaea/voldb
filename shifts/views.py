028a69c1096586cb997e8c57787128f20c2c8094
Koda <zaskoda@gmail.com>
2014-02-15 14:51:23 -0700

    shifts update

--- shifts/views.py
+++ shifts/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from shifts.models import Shift
+from departments.models import Department
 
 
 # Create your views here.
 
 def index(request):
-    shift_list = Shift.objects.all()  
-    return render(request, 'shifts/index.html', {'shift_list': shift_list})    
+    department_list = Department.objects.order_by('name').values('name','id')
+    depts = ""
+    for dept in department_list:
+        dname = dept["name"]
+        shift_list += {dept["name"],  Shift.objects.order_by('department', 'start_time').filter(department=dept["id"])}
+    return render(request, 'shifts/index.html', {'shift_list': shift_list, 'department_list' : department_list})    
