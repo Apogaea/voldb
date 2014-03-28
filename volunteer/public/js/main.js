a947bebd49b324e6d88f85886f4e14c198c8fe42
Koda <zaskoda@gmail.com>
2014-03-28 05:08:00 -0600

    I forgot some assets and made a few more edits

--- volunteer/public/js/main.js
+++ volunteer/public/js/main.js
@@ -54,16 +54,16 @@ $(document).ready( function () {
 
     /*
      *  Parallax Scrolling
-     */
-    $(window).scroll(function(){
-        parallaxScroll();
-    });
+     */    
+    if (!isTouchDevice()) { 
+        $(window).scroll(function(){
+            parallaxScroll();
+        });
+    }
 
     function parallaxScroll(){
-        var scrolled = $(window).scrollTop();
-        //$("#parallax-bg1").css("top",(0-(scrolled*.25))+"px");
-        //$(window).css("background-position-y", (0-(scrolled*1.25))+"px");            
-        $("body").css("background-position", "center "+(0-(scrolled*0.45))+"px");
+        var scrolled = $(window).scrollTop();      
+        $("body").css("background-position", "center "+((scrolled*0.25))+"px");
     }
 
     /*
@@ -151,6 +151,12 @@ $(document).ready( function () {
         });
     }
 
-    parallaxScroll();
+    function isTouchDevice() {
+       var el = document.createElement('div');
+       el.setAttribute('ongesturestart', 'return;'); // or try "ontouchstart"
+       return typeof el.ongesturestart === "function";
+    }
+
+    if (!isTouchDevice()) { parallaxScroll(); }
     bindToClaimShift();
 });
