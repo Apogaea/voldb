b4571d71c2f3d04c767ae3927d2f3c29aa48ba58
Piper Merriam <piper@simpleenergy.com>
2014-03-28 22:38:28 -0600

    remove sslify and forcing ssl

--- volunteer/settings_heroku.py
+++ volunteer/settings_heroku.py
@@ -9,18 +9,8 @@ from herokuify.aws import *  # NOQA
 
 from volunteer.settings_aws import *  # NOQA
 
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-
 DATABASES = herokuify.get_db_config()
 CACHES = herokuify.get_cache_config()
 
 DEBUG = os.environ.get('DJANGO_DEBUG') == 'True'
 TEMPLATE_DEBUG = DEBUG
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-MIDDLEWARE_CLASSES = (
-    'sslify.middleware.SSLifyMiddleware',
-) + MIDDLEWARE_CLASSES
