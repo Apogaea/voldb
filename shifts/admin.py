7185e09b95ecccbafd43243e3df1009a1eb5ba15
Koda <zaskoda@gmail.com>
2014-03-29 02:36:37 -0600

    added ability to clear codes in bulk

--- shifts/admin.py
+++ shifts/admin.py
@@ -7,12 +7,12 @@ class ShiftAdmin(admin.ModelAdmin):
     list_filter = ['shift_length', 'department', 'code', 'start_time']
     actions = ['clear_code']
     def clear_code(self, request, queryset):
-        rows_updated = queryset.update(code = '')
+        rows_updated = queryset.update(code='')
         if rows_updated == 1:
             message_bit = "1 code was"
         else:
             message_bit = "%s codes were" % rows_updated
-        self.message_user(request, "%s cleared." % message_bit)        
+        self.message_user(request, "%s cleared." % message_bit)
     clear_code.short_description = "Clear codes for selected shifts"
 
 
