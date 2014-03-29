f116d62a57ebf400f410b37e19b96760aa32997f
Koda <zaskoda@gmail.com>
2014-03-29 07:12:14 -0600

    significant round of styles and fixes

--- departments/admin.py
+++ departments/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from departments.models import Department
 
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
 
-admin.site.register(Department)
+admin.site.register(Department,DepartmentAdmin)
