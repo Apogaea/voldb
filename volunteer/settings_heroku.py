01daa9d383c6a6bd251bf88f6cef00abefcef954
Piper Merriam <piper@simpleenergy.com>
2014-03-31 21:47:03 -0600

    fix template loaders

--- volunteer/settings_heroku.py
+++ volunteer/settings_heroku.py
@@ -19,3 +19,10 @@ TEMPLATE_DEBUG = DEBUG
 RAVEN_CONFIG = {
     'dsn': os.environ['SENTRY_DSN'],
 }
+
+TEMPLATE_LOADERS = (
+    ('django.template.loaders.cached.Loader', (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    )),
+)
