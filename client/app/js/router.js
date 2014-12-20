d9003a19c1bd813421966685b272830f637367a4
Dan Singer <devnill@gmail.com>
2014-12-20 12:15:32 -0700

    updated pathing

--- client/app/js/router.js
+++ client/app/js/router.js
@@ -2,13 +2,19 @@ define(['backbone','appView'],function(Backbone,AppView){
 
   var AppRouter = Backbone.Router.extend({
     routes: {
-      "*actions": "defaultRoute"
+      "*actions": "defaultRoute",
+      "user/:id": "viewUser"
     }
   });
   
   AppRouter = new AppRouter;
   AppRouter.on('route:defaultRoute', function () {
-    window.volDB=new AppView();
+    if(!window.volDB){
+        window.volDB=new AppView();
+    } 
+  });
+  AppRouter.on('route:viewUser', function () {
+    console.log('hi there user');
   });
   Backbone.history.start();
   return AppRouter;
