f79eb85f0ce84ef4af9da24213dc8bb3ae106c03
Dan Singer <devnill@gmail.com>
2014-12-31 19:16:03 -0700

    added files for shift grid

---
+++ client/app/js/views/shiftDetails.js
@@ -0,0 +1,16 @@
+/*global require, define, utils
+//todo this is for the details modal later on.
+*/ 
+define([
+  'text!../templates/shiftDetails.html'
+],function(shiftDetails){  
+   var ShiftDetails=Backbone.View.extend({
+     initialize:function(){
+
+     },
+     render:function(){
+
+     }
+   });
+});
+
