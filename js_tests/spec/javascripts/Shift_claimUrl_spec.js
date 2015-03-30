096bae9e16f2699956ef49e0e02ee2742532e073
Piper Merriam <piper@simpleenergy.com>
2015-03-30 09:44:42 -0600

    fix typo

--- js_tests/spec/javascripts/Shift_claimUrl_spec.js
+++ js_tests/spec/javascripts/Shift_claimUrl_spec.js
@@ -1,4 +1,4 @@
-describe("Shift.cliamUrl", function() {
+describe("Shift.claimUrl", function() {
   it("should be the right url", function() {
     var shift = new app.Shift({id: 3});
     expect(shift.claimUrl()).toEqual("/api/v2/shifts/3/claim/");
