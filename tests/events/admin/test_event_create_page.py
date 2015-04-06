4f19199ceb935d4a476b19a2074945a97fc55608
Piper Merriam <piper@simpleenergy.com>
2015-04-06 14:00:37 -0600

    finish event tests

--- tests/events/admin/test_event_create_page.py
+++ tests/events/admin/test_event_create_page.py
@@ -1,9 +1,25 @@
+from django.utils import timezone
 from django.core.urlresolvers import reverse
+from django.forms.utils import (
+    to_current_timezone,
+)
 
 
-def test_event_list_page(admin_webtest_client):
+def test_event_list_page(admin_webtest_client, models):
     url = reverse('admin:event-create')
     response = admin_webtest_client.get(url)
     assert response.status_code == 200
 
+    open_at = timezone.now().replace(microsecond=0) + timezone.timedelta(10)
+    close_at = timezone.now().replace(microsecond=0) + timezone.timedelta(20)
+
     response.form['name'] = 'Test Event'
+    response.form['registration_open_at'] = to_current_timezone(open_at).strftime('%Y-%m-%d %H:%M:%S')
+    response.form['registration_close_at'] = to_current_timezone(close_at).strftime('%Y-%m-%d %H:%M:%S')
+
+    form_response = response.form.submit()
+    assert form_response.status_code == 302
+
+    created_event = models.Event.objects.get(name='Test Event')
+    assert created_event.registration_open_at == open_at
+    assert created_event.registration_close_at == close_at
