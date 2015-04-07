acaf73319418c21ca882be59f4c2badbd0479e51
Piper Merriam <piper@simpleenergy.com>
2015-04-07 11:39:38 -0400

    Add js rollbar tracking

--- volunteer/settings.py
+++ volunteer/settings.py
@@ -42,6 +42,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.tz',
     'django.contrib.messages.context_processors.messages',
     'django.core.context_processors.request',
+    'volunteer.coor.context_processors.rollbar',
 )
 
 # Allowed Hosts
