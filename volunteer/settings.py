50f17d59fac87e3a84a339559808f988f295698d
Piper Merriam <piper@simpleenergy.com>
2015-04-07 11:43:48 -0400

    fix bad path

--- volunteer/settings.py
+++ volunteer/settings.py
@@ -42,7 +42,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.tz',
     'django.contrib.messages.context_processors.messages',
     'django.core.context_processors.request',
-    'volunteer.coor.context_processors.rollbar',
+    'volunteer.core.context_processors.rollbar',
 )
 
 # Allowed Hosts
