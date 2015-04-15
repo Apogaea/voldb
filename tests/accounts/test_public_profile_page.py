70a3ceb03e0cbb57847cabe07ac33001fb4105e9
Piper Merriam <piper@simpleenergy.com>
2015-04-15 12:45:06 -0600

    fix test

--- tests/accounts/test_public_profile_page.py
+++ tests/accounts/test_public_profile_page.py
@@ -9,4 +9,4 @@ def test_public_profile_display(user_webtest_client, user, factories):
     response = user_webtest_client.get(reverse('public-profile', kwargs={'pk': other_user.pk}))
     assert response.status_code == status.HTTP_200_OK
 
-    assert other_user.display_name in response.content
+    assert other_user.profile.display_name in response.content
